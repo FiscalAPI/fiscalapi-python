8fb114e0a4a83d36d0ace022a6f8ac82b9d30a51
JesusMendoza <mendoza.git@gmail.com>
2025-02-06 19:15:37 -0600

    catalog service added

--- fiscalapi/services/fiscalapi_client.py
+++ fiscalapi/services/fiscalapi_client.py
@@ -1,4 +1,5 @@
 from fiscalapi.models.common_models import FiscalApiSettings
+from fiscalapi.services.catalog_service import CatalogService
 from fiscalapi.services.people_service import PeopleService
 from fiscalapi.services.product_service import ProductService
 from fiscalapi.services.tax_file_servive import TaxFileService
@@ -11,7 +12,5 @@ class FiscalApiClient:
         self.products = ProductService(settings)
         self.people = PeopleService(settings)
         self.tax_files = TaxFileService(settings)
-        
-        
-        #self.invoice_service = InvoiceService(settings)
-        # Otros servicios concretos pueden ser inicializados aquí
+        self.catalogs = CatalogService(settings)
+        self.settings = settings
