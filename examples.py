427694258b3a12d3e583511a70167dbaa8cda21e
JesusMendoza <mendoza.git@gmail.com>
2025-02-10 20:21:34 -0600

    samples updated

--- examples.py
+++ examples.py
@@ -191,14 +191,82 @@ def main ():
     #print(api_response)
     
     
-    invoice = Invoice(
+    # invoice = Invoice(
+    #     version_code="4.0",
+    #     series="F",
+    #     date=datetime.now().strftime("%Y-%m-%dT%H:%M:%S"), #AAAA-MM-DDThh:mm:ss
+    #     payment_form_code="01",
+    #     payment_conditions="Contado",
+    #     currency_code="MXN",
+    #     type_code="I",
+    #     expedition_zip_code="01160",
+    #     payment_method_code="PUE",
+    #     exchange_rate=1,
+    #     export_code="01",
+    #     issuer=InvoiceIssuer(
+    #         tin="FUNK671228PH6",
+    #         legal_name="KARLA FUENTE NOLASCO",
+    #         tax_regime_code="621",
+    #         tax_credentials= [
+    #             TaxCredential(
+    #                 base64_file="MIIFgDCCA2igAwIBAgIUMzAwMDEwMDAwMDA1MDAwMDM0NDYwDQYJKoZIhvcNAQELBQAwggErMQ8wDQYDVQQDDAZBQyBVQVQxLjAsBgNVBAoMJVNFUlZJQ0lPIERFIEFETUlOSVNUUkFDSU9OIFRSSUJVVEFSSUExGjAYBgNVBAsMEVNBVC1JRVMgQXV0aG9yaXR5MSgwJgYJKoZIhvcNAQkBFhlvc2Nhci5tYXJ0aW5lekBzYXQuZ29iLm14MR0wGwYDVQQJDBQzcmEgY2VycmFkYSBkZSBjYWxpejEOMAwGA1UEEQwFMDYzNzAxCzAJBgNVBAYTAk1YMRkwFwYDVQQIDBBDSVVEQUQgREUgTUVYSUNPMREwDwYDVQQHDAhDT1lPQUNBTjERMA8GA1UELRMIMi41LjQuNDUxJTAjBgkqhkiG9w0BCQITFnJlc3BvbnNhYmxlOiBBQ0RNQS1TQVQwHhcNMjMwNTE4MTQzNTM3WhcNMjcwNTE4MTQzNTM3WjCBpzEdMBsGA1UEAxMUS0FSTEEgRlVFTlRFIE5PTEFTQ08xHTAbBgNVBCkTFEtBUkxBIEZVRU5URSBOT0xBU0NPMR0wGwYDVQQKExRLQVJMQSBGVUVOVEUgTk9MQVNDTzEWMBQGA1UELRMNRlVOSzY3MTIyOFBINjEbMBkGA1UEBRMSRlVOSzY3MTIyOE1DTE5MUjA1MRMwEQYDVQQLEwpTdWN1cnNhbCAxMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAhNXbTSqGX6+/3Urpemyy5vVG2IdP2v7v001+c4BoMxEDFDQ32cOFdDiRxy0Fq9aR+Ojrofq8VeftvN586iyA1A6a0QnA68i7JnQKI4uJy+u0qiixuHu6u3b3BhSpoaVHcUtqFWLLlzr0yBxfVLOqVna/1/tHbQJg9hx57mp97P0JmXO1WeIqi+Zqob/mVZh2lsPGdJ8iqgjYFaFn9QVOQ1Pq74o1PTqwfzqgJSfV0zOOlESDPWggaDAYE4VNyTBisOUjlNd0x7ppcTxSi3yenrJHqkq/pqJsRLKf6VJ/s9p6bsd2bj07hSDpjlDC2lB25eEfkEkeMkXoE7ErXQ5QCwIDAQABox0wGzAMBgNVHRMBAf8EAjAAMAsGA1UdDwQEAwIGwDANBgkqhkiG9w0BAQsFAAOCAgEAHwYpgbClHULXYhK4GNTgonvXh81oqfXwCSWAyDPiTYFDWVfWM9C4ApxMLyc0XvJte75Rla+bPC08oYN3OlhbbvP3twBL/w9SsfxvkbpFn2ZfGSTXZhyiq4vjmQHW1pnFvGelwgU4v3eeRE/MjoCnE7M/Q5thpuog6WGf7CbKERnWZn8QsUaJsZSEkg6Bv2jm69ye57ab5rrOUaeMlstTfdlaHAEkUgLX/NXq7RbGwv82hkHY5b2vYcXeh34tUMBL6os3OdRlooN9ZQGkVIISvxVZpSHkYC20DFNh1Bb0ovjfujlTcka81GnbUhFGZtRuoVQ1RVpMO8xtx3YKBLp4do3hPmnRCV5hCm43OIjYx9Ov2dqICV3AaNXSLV1dW39Bak/RBiIDGHzOIW2+VMPjvvypBjmPv/tmbqNHWPSAWOxTyMx6E1gFCZvi+5F+BgkdC3Lm7U0BU0NfvsXajZd8sXnIllvEMrikCLoI/yurvexNDcF1RW/FhMsoua0eerwczcNm66pGjHm05p9DR6lFeJZrtqeqZuojdxBWy4vH6ghyJaupergoX+nmdG3JYeRttCFF/ITI68TeCES5V3Y0C3psYAg1XxcGRLGd4chPo/4xwiLkijWtgt0/to5ljGBwfK7r62PHZfL1Dp+i7V3w7hmOlhbXzP+zhMZn1GCk7KY=",
+    #                 file_type=0,
+    #                 password="12345678a"
+    #             ),
+    #             TaxCredential(
+    #                 base64_file="MIIFDjBABgkqhkiG9w0BBQ0wMzAbBgkqhkiG9w0BBQwwDgQIAgEAAoIBAQACAggAMBQGCCqGSIb3DQMHBAgwggS9AgEAMASCBMh4EHl7aNSCaMDA1VlRoXCZ5UUmqErAbucRBAKNQXH8t8gVCl/ItHMI2hMJ76QOECOqEi1Y89cDpegDvh/INXyMsXbzi87tfFzgq1O+9ID6aPWGg+bNGADXyXxDVdy7Nq/SCdoXvo66MTYwq8jyJeUHDHEGMVBcmZpD44VJCvLBxDcvByuevP4Wo2NKqJCwK+ecAdZc/8Rvd947SjbMHuS8BppfQWARVUqA5BLOkTAHNv6tEk/hncC7O2YOGSShart8fM8dokgGSyewHVFe08POuQ+WDHeVpvApH/SP29rwktSoiHRoL6dK+F2YeEB5SuFW9LQgYCutjapmUP/9TC3Byro9Li6UrvQHxNmgMFGQJSYjFdqlGjLibfuguLp7pueutbROoZaSxU8HqlfYxLkpJUxUwNI1ja/1t3wcivtWknVXBd13R06iVfU1HGe8Kb4u5il4a4yP4p7VT4RE3b1SBLJeG+BxHiE8gFaaKcX/Cl6JV14RPTvk/6VnAtEQ66qHJex21KKuiJo2JoOmDXVHmvGQlWXNjYgoPx28Xd5WsofL+n7HDR2Ku8XgwJw6IXBJGuoday9qWN9v/k7DGlNGB6Sm4gdVUmycMP6EGhB1vFTiDfOGQO42ywmcpKoMETPVQ5InYKE0xAOckgcminDgxWjtUHjBDPEKifEjYudPwKmR6Cf4ZdGvUWwY/zq9pPAC9bu423KeBCnSL8AQ4r5SVsW6XG0njamwfNjpegwh/YG7sS7sDtZ8gi7r6tZYjsOqZlCYU0j7QTBpuQn81Yof2nQRCFxhRJCeydmIA8+z0nXrcElk7NDPk4kYQS0VitJ2qeQYNENzGBglROkCl2y6GlxAG80IBtReCUp/xOSdlwDR0eim+SNkdStvmQM5IcWBuDKwGZc1A4v/UoLl7niV9fpl4X6bUX8lZzY4gidJOafoJ30VoY/lYGkrkEuz3GpbbT5v8fF3iXVRlEqhlpe8JSGu7Rd2cPcJSkQ1Cuj/QRhHPhFMF2KhTEf95c9ZBKI8H7SvBi7eLXfSW2Y0ve6vXBZKyjK9whgCU9iVOsJjqRXpAccaWOKi420CjmS0+uwj/Xr2wLZhPEjBA/G6Od30+eG9mICmbp/5wAGhK/ZxCT17ZETyFmOMo49jl9pxdKocJNuzMrLpSz7/g5Jwp8+y8Ck5YP7AX0R/dVA0t37DO7nAbQT5XVSYpMVh/yvpYJ9WR+tb8Yg1h2lERLR2fbuhQRcwmisZR2W3Sr2b7hX9MCMkMQw8y2fDJrzLrqKqkHcjvnI/TdzZW2MzeQDoBBb3fmgvjYg07l4kThS73wGX992w2Y+a1A2iirSmrYEm9dSh16JmXa8boGQAONQzQkHh7vpw0IBs9cnvqO1QLB1GtbBztUBXonA4TxMKLYZkVrrd2RhrYWMsDp7MpC4M0p/DA3E/qscYwq1OpwriewNdx6XXqMZbdUNqMP2viBY2VSGmNdHtVfbN/rnaeJetFGX7XgTVYD7wDq8TW9yseCK944jcT+y/o0YiT9j3OLQ2Ts0LDTQskpJSxRmXEQGy3NBDOYFTvRkcGJEQJItuol8NivJN1H9LoLIUAlAHBZxfHpUYx66YnP4PdTdMIWH+nxyekKPFfAT7olQ=",
+    #                 file_type=1,
+    #                 password="12345678a"
+    #             )
+    #         ]
+    #     ),
+    #     recipient=InvoiceRecipient(
+    #         tin="EKU9003173C9",
+    #         legal_name="ESCUELA KEMPER URGATE",
+    #         zip_code="42501",
+    #         tax_regime_code="601",
+    #         cfdi_use_code="G01",
+    #         email="mail@domain.com",
+    #     ),
+        
+    #     items=[
+    #         InvoiceItem(
+    #             item_code="01010101",
+    #             quantity= Decimal("9.5"),
+    #             unit_of_measurement_code="E48",
+    #             unit_of_measurement="Unidad de servicio",
+    #             description="Invoicing software as a service",
+    #             unit_price=Decimal("3587.75"),
+    #             tax_object_code="02",
+    #             item_sku="7506022301697",
+    #             discount=Decimal("255.85"),
+    #             item_taxes=[
+    #                 ItemTax(
+    #                     tax_code="002",
+    #                     tax_type_code="Tasa",
+    #                     tax_rate=Decimal("0.160000"),
+    #                     tax_flag_code="T"
+    #                 )
+    #             ]
+    #         )
+    #     ]
+    # )
+    
+    # # timbrar factura
+    # api_response = client.invoices.create(invoice)    
+    # print(api_response)
+    
+    
+    
+    # Crear nota de credito por valores
+    credit_note = Invoice(
         version_code="4.0",
         series="F",
         date=datetime.now().strftime("%Y-%m-%dT%H:%M:%S"), #AAAA-MM-DDThh:mm:ss
         payment_form_code="01",
         payment_conditions="Contado",
         currency_code="MXN",
-        type_code="I",
+        type_code="E",
         expedition_zip_code="01160",
         payment_method_code="PUE",
         exchange_rate=1,
@@ -228,18 +296,23 @@ def main ():
             cfdi_use_code="G01",
             email="mail@domain.com",
         ),
+        related_invoices=[
+            RelatedInvoice(
+                uuid="05341ec6-538d-4d92-938e-acc9b33da47e",
+                relationship_type_code="01"
+            )
+        ],
         
         items=[
             InvoiceItem(
                 item_code="01010101",
-                quantity= Decimal("9.5"),
+                quantity= Decimal("0.5"),
                 unit_of_measurement_code="E48",
                 unit_of_measurement="Unidad de servicio",
                 description="Invoicing software as a service",
                 unit_price=Decimal("3587.75"),
                 tax_object_code="02",
                 item_sku="7506022301697",
-                discount=Decimal("255.85"),
                 item_taxes=[
                     ItemTax(
                         tax_code="002",
@@ -249,13 +322,13 @@ def main ():
                     )
                 ]
             )
-        ]
+        ]    
     )
+        
+    # timbrar nota de credito
+    api_response = client.invoices.create(credit_note)    
     
-    # timbrar factura
-    api_response = client.invoices.create(invoice)    
     print(api_response)
-    
     
     
 if __name__ == "__main__":
